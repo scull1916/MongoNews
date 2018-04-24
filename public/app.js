c6d16786d914e7882387854dce9fb12f1df641fa
scull1916 <kpscully@gmail.com>
2018-04-23 23:36:06 -0400

    wrangling with pulling sibling elements from HTML

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + "</p>");
   }
 });
 
